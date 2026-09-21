7cea5c792b032c72f7d9c1551505bf764e38d581
cristian-808 <cgaraviz@unimagdalena.edu.co>
2026-09-20 20:19:13 -0500

    solucion de error, rama temporal

--- src/main/resources/db/migration/V2__insert_initial_artists.sql
+++ src/main/resources/db/migration/V2__insert_initial_artists.sql
@@ -1,8 +1,8 @@
-INSERT INTO artists (stage_name)
+INSERT INTO artists (stage_name, country, genre, active)
 VALUES
     ('Solar Beat', 'Colombia' , 'Champewave' , TRUE),
     ('Neon Waves', 'Argentina', 'Electro pop', TRUE),
     ('Caribbean Sound', 'Jamaica', 'Reggae', TRUE),
     ('Ocean Drive', 'USA', 'Pop', TRUE),
-    ('Digital Pulse', 'Germany', 'Techno', TRUE);
+    ('Digital Pulse', 'Alemania', 'Techno', TRUE);
 
