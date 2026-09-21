0fe03be09130e2d89ae49cd5dffbd0c26b2a2b8c
cristian-808 <cgaraviz@unimagdalena.edu.co>
2026-09-20 19:58:48 -0500

    feature(database): insert initial artists

---
+++ src/main/resources/db/migration/V2__insert_initial_artists.sql
@@ -0,0 +1,8 @@
+INSERT INTO artists (stage_name)
+VALUES
+    ('Solar Beat', 'Colombia' , 'Champewave' , TRUE),
+    ('Neon Waves', 'Argentina', 'Electro pop', TRUE),
+    ('Caribbean Sound', 'Jamaica', 'Reggae', TRUE),
+    ('Ocean Drive', 'USA', 'Pop', TRUE),
+    ('Digital Pulse', 'Germany', 'Techno', TRUE);
+
